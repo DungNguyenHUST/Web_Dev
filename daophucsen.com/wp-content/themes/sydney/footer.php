73dd218524fe01b8994f59e59b94b3318f3ba634
Dung Nguyen <vandungbknd@gmail.com>
2019-05-14 19:47:16 +0700

    add web daophucsen

--- daophucsen.com/wp-content/themes/sydney/footer.php
+++ daophucsen.com/wp-content/themes/sydney/footer.php
@@ -21,9 +21,9 @@
 		
 	<footer id="colophon" class="site-footer" role="contentinfo">
 		<div class="site-info container">
-			<a href="<?php echo esc_url( __( 'https://wordpress.org/', 'sydney' ) ); ?>"><?php printf( __( 'Proudly powered by %s', 'sydney' ), 'WordPress' ); ?></a>
-			<span class="sep"> | </span>
-			<?php printf( __( 'Theme: %2$s by %1$s.', 'sydney' ), 'aThemes', '<a href="https://athemes.com/theme/sydney" rel="nofollow">Sydney</a>' ); ?>
+			<a href="<?php echo esc_url( __( 'https://daophucsencaocap.com/', 'sydney' ) ); ?>"><?php printf( __( 'Copyright © 2019 %s', 'Dao Phuc Sen Cao Cap Viet Nam' ), 'Dao Phuc Sen Cao Cap Viet Nam' ); ?></a>
+			<!-- <span class="sep"> | </span> -->
+			<!-- <?php printf( __( 'Theme: %2$s by %1$s.', 'sydney' ), 'aThemes', '<a href="https://athemes.com/theme/sydney" rel="designer">Sydney</a>' ); ?> -->
 		</div><!-- .site-info -->
 	</footer><!-- #colophon -->
 
